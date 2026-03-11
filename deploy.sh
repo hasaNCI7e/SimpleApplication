927cd5d30dff647fe68c852d135321cd5dfef783
Hasan7e <hasan7e@gmail.com>
2026-03-11 16:15:41 +0000

    minor update

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 sudo apt update $$ sudo apt install nodejs npm 
 sudo npm install -g pm2
-cd SimpleApplicaiton
+pm2 stop SimpleApplicaiton
+cd SimpleApplicaiton/
 npm install
 pm2 start ./bin www --name "simple_application"
