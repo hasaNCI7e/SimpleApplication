2e42c0a59acacda33ba797accb4f7d45675ee7a5
Hasan7e <hasan7e@gmail.com>
2026-03-11 16:27:36 +0000

    fixed more typo

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-sudo apt update $$ sudo apt install nodejs npm 
+sudo apt update && sudo apt install -y nodejs npm 
 sudo npm install -g pm2
-pm2 stop SimpleApplicaiton
-cd SimpleApplicaiton/
+pm2 delete simple_application || true
+cd SimpleApplication/
 npm install
-pm2 start ./bin www --name "simple_application"
+pm2 start ./bin/www --name "simple_application"
