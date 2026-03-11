ba0f963c1d3eee83749e48dec41013e0dcd62563
Hasan7e <hasan7e@gmail.com>
2026-03-11 22:53:23 +0000

    updated deploy.sh again

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
+cd /home/ubuntu || exit 1
 sudo apt update && sudo apt install -y nodejs npm 
 sudo npm install -g pm2
-pm2 delete simple_application || true
-cd SimpleApplication/
+
+cd /home/ubuntu/SimpleApplication || exit 1
+
 npm install
 echo "$privatekey" > privatekey.pem
 echo "$server" > server.crt
 
 chmod 600 privatekey.pem
 chmod 644 server.crt
+pm2 delete simple_application || true
 pm2 start ./bin/www --name "simple_application"
 
-# another test agian 
+# another test on deploy.sh
