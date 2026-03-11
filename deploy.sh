d866845766d5326a0e1c03d456b94ceac81d0bff
Hasan7e <hasan7e@gmail.com>
2026-03-11 19:44:48 +0000

    added HTTPS for secure communication

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,6 @@ sudo npm install -g pm2
 pm2 delete simple_application || true
 cd SimpleApplication/
 npm install
+echo $PRIVATE_KEY > private_key.pem
+echo &SERVER_KEY > server_key.crt
 pm2 start ./bin/www --name "simple_application"
